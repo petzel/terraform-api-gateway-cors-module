a7dec619661571fc35324e37a76de925a48e712d
Eric Petzel <epetzel@gmail.com>
2017-09-30 04:23:55 -0500

    remove undefined methods from integration response methods

--- main.tf
+++ main.tf
@@ -24,7 +24,8 @@ resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationRespo
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
+#    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET'",
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
 }
